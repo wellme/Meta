0413289f4eb4366c5b59801f07c403fdbbaf9786
wellme <wellmey@gmail.com>
2019-09-28 01:01:51 -0400

    Correction of substractions

--- Meta/Meta/BinInt/BinInt.not.cpp
+++ Meta/Meta/BinInt/BinInt.not.cpp
@@ -1,5 +1,5 @@
-
 #pragma once
+
 #include "BinInt.cpp"
 
 namespace Meta::BinInt {
--- Meta/Meta/BinInt/BinInt.xor.cpp
+++ Meta/Meta/BinInt/BinInt.xor.cpp
@@ -1,25 +1,31 @@
 #pragma once
 
 #include "BinInt.cpp"
+#include "BinInt.not.cpp"
 
 namespace Meta::BinInt {
 
 	template<typename T1, typename T2>
 	struct Xor;
 
-	template<>
-	struct Xor<void, void> {
-		using type = void;
+	template<typename T>
+	struct Xor<BinIntFalse<void>, BinIntTrue<T>> {
+		using type = BinIntFalse<void>;
 	};
 
 	template<typename T>
-	struct Xor<T, void> {
-		using type = T;
+	struct Xor<BinIntFalse<void>, BinIntFalse<T>> {
+		using type = BinIntFalse<T>;
 	};
 
 	template<typename T>
-	struct Xor<void, T> {
-		using type = T;
+	struct Xor<BinIntTrue<void>, BinIntTrue<T>> {
+		using type = BinIntFalse<typename Not<T>::type>;
+	};
+
+	template<typename T>
+	struct Xor<BinIntTrue<void>, BinIntFalse<T>> {
+		using type = BinIntTrue<typename Not<T>::type>;
 	};
 
 
@@ -41,5 +47,10 @@ namespace Meta::BinInt {
 	template<typename T1, typename T2>
 	struct Xor<BinIntTrue<T1>, BinIntTrue<T2>> {
 		using type = BinIntFalse<typename Xor<T1, T2>::type>;
+	};
+
+	template<typename T1, typename T2>
+	struct Xor {
+		using type = typename Xor<T2, T1>::type;
 	};
 }
