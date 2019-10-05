2c918ea35513dc8845eadc0097f405dff686c467
wellme <wellmey@gmail.com>
2019-10-05 16:38:42 -0400

    Reimplemented Xor

--- Meta/Meta/BinInt/BinInt.xor.cpp
+++ Meta/Meta/BinInt/BinInt.xor.cpp
@@ -2,55 +2,46 @@
 
 #include "BinInt.cpp"
 #include "BinInt.not.cpp"
+#include "BinInt.lengthen.cpp"
 
 namespace Meta::BinInt {
 
-	template<typename T1, typename T2>
-	struct Xor;
+	namespace XorInternals {
 
-	template<typename T>
-	struct Xor<BinIntFalse<void>, BinIntTrue<T>> {
-		using type = BinIntFalse<void>;
-	};
+		template<typename T1, typename T2>
+		struct Xor;
 
-	template<typename T>
-	struct Xor<BinIntFalse<void>, BinIntFalse<T>> {
-		using type = BinIntFalse<T>;
-	};
-
-	template<typename T>
-	struct Xor<BinIntTrue<void>, BinIntTrue<T>> {
-		using type = BinIntFalse<typename Not<T>::type>;
-	};
-
-	template<typename T>
-	struct Xor<BinIntTrue<void>, BinIntFalse<T>> {
-		using type = BinIntTrue<typename Not<T>::type>;
-	};
+		template<>
+		struct Xor<void, void> {
+			using type = void;
+		};
 
 
-	template<typename T1, typename T2>
-	struct Xor<BinIntFalse<T1>, BinIntFalse<T2>> {
-		using type = BinIntFalse<typename Xor<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct Xor<BinIntFalse<T1>, BinIntFalse<T2>> {
+			using type = BinIntFalse<typename Xor<T1, T2>::type>;
+		};
 
-	template<typename T1, typename T2>
-	struct Xor<BinIntFalse<T1>, BinIntTrue<T2>> {
-		using type = BinIntTrue<typename Xor<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct Xor<BinIntFalse<T1>, BinIntTrue<T2>> {
+			using type = BinIntTrue<typename Xor<T1, T2>::type>;
+		};
 
-	template<typename T1, typename T2>
-	struct Xor<BinIntTrue<T1>, BinIntFalse<T2>> {
-		using type = BinIntTrue<typename Xor<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct Xor<BinIntTrue<T1>, BinIntFalse<T2>> {
+			using type = BinIntTrue<typename Xor<T1, T2>::type>;
+		};
 
-	template<typename T1, typename T2>
-	struct Xor<BinIntTrue<T1>, BinIntTrue<T2>> {
-		using type = BinIntFalse<typename Xor<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct Xor<BinIntTrue<T1>, BinIntTrue<T2>> {
+			using type = BinIntFalse<typename Xor<T1, T2>::type>;
+		};
+	}
 
 	template<typename T1, typename T2>
 	struct Xor {
-		using type = typename Xor<T2, T1>::type;
+		using lengthener = Lengthen<T1, T2>;
+		using type = typename XorInternals::Xor<typename lengthener::type1, typename lengthener::type2>::type;
 	};
+
 }
