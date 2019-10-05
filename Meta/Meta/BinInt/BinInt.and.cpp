131b9bee54ab0acc66f9632f369fcc426625e9cc
wellme <wellmey@gmail.com>
2019-10-05 16:12:20 -0400

    Reimplemented And

--- Meta/Meta/BinInt/BinInt.and.cpp
+++ Meta/Meta/BinInt/BinInt.and.cpp
@@ -1,54 +1,44 @@
 #pragma once
 
 #include "BinInt.cpp"
+#include "BinInt.lengthen.cpp"
 
 namespace Meta::BinInt {
 
-	template<typename T1, typename T2>
-	struct And;
+	namespace AndInternals {
 
-	template<typename T>
-	struct And<BinIntFalse<void>, BinIntTrue<T>> {
-		using type = BinIntFalse<void>;
-	};
+		template<typename T1, typename T2>
+		struct And;
 
-	template<typename T>
-	struct And<BinIntFalse<void>, BinIntFalse<T>> {
-		using type = BinIntFalse<void>;
-	};
+		template<>
+		struct And<void, void> {
+			using type = void;
+		};
 
-	template<typename T>
-	struct And<BinIntTrue<void>, BinIntTrue<T>> {
-		using type = BinIntTrue<T>;
-	};
+		template<typename T1, typename T2>
+		struct And<BinIntFalse<T1>, BinIntFalse<T2>> {
+			using type = BinIntFalse<typename And<T1, T2>::type>;
+		};
 
-	template<typename T>
-	struct And<BinIntTrue<void>, BinIntFalse<T>> {
-		using type = BinIntFalse<T>;
-	};
+		template<typename T1, typename T2>
+		struct And<BinIntFalse<T1>, BinIntTrue<T2>> {
+			using type = BinIntFalse<typename And<T1, T2>::type>;
+		};
 
-	template<typename T1, typename T2>
-	struct And<BinIntFalse<T1>, BinIntFalse<T2>> {
-		using type = BinIntFalse<typename And<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct And<BinIntTrue<T1>, BinIntFalse<T2>> {
+			using type = BinIntFalse<typename And<T1, T2>::type>;
+		};
 
-	template<typename T1, typename T2>
-	struct And<BinIntFalse<T1>, BinIntTrue<T2>> {
-		using type = BinIntFalse<typename And<T1, T2>::type>;
-	};
-
-	template<typename T1, typename T2>
-	struct And<BinIntTrue<T1>, BinIntFalse<T2>> {
-		using type = BinIntFalse<typename And<T1, T2>::type>;
-	};
-
-	template<typename T1, typename T2>
-	struct And<BinIntTrue<T1>, BinIntTrue<T2>> {
-		using type = BinIntTrue<typename And<T1, T2>::type>;
-	};
+		template<typename T1, typename T2>
+		struct And<BinIntTrue<T1>, BinIntTrue<T2>> {
+			using type = BinIntTrue<typename And<T1, T2>::type>;
+		};
+	}
 
 	template<typename T1, typename T2>
 	struct And {
-		using type = typename And<T2, T1>::type;
+		using lengthener = Lengthen<T1, T2>;
+		using type = typename AndInternals::And<typename lengthener::type1, typename lengthener::type2>::type;
 	};
 }
