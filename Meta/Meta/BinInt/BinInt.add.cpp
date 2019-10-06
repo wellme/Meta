18b45697c4b3d0a10501ac4c69573590889e2b99
wellme <wellmey@gmail.com>
2019-10-06 17:27:16 -0400

    Reimplemented add

--- Meta/Meta/BinInt/BinInt.add.cpp
+++ Meta/Meta/BinInt/BinInt.add.cpp
@@ -1,26 +1,35 @@
 #pragma once
 
 #include "BinInt.cpp"
-#include "BinInt.simplify.cpp"
 #include "BinInt.xor.cpp"
 #include "BinInt.and.cpp"
+#include "BinInt.is_null.cpp"
+#include "BinInt.shift.cpp"
 
 namespace Meta::BinInt {
 	
-	template<typename T1, typename T2>
-	struct Add;
+	namespace AddInternals {
 
-	template<typename T1>
-	struct Add<T1, BinIntFalse<void>> {
-		using type = T1;
-	};
+		template<typename T1, typename T2, typename Remaining>
+		struct Loop;
+
+		template<typename T1, typename T2>
+		struct Loop<T1, T2, void> {
+			using type = T1;
+		};
+
+		template<typename T1, typename T2, template<typename> typename T3, typename T3Arg>
+		struct Loop<T1, T2, T3<T3Arg>> {
+			using xor = typename Xor<T1, T2>::type;
+			using and = typename And<T1, T2>::type;
+			using shifted = typename ShiftLeftOne<and>::type;
+			using type = typename Loop<xor, shifted, T3Arg>::type;
+		};
+	}
 
 	template<typename T1, typename T2>
 	struct Add {
-		using xor = typename Xor<T1, T2>::type;
-		using and = typename And<T1, T2>::type;
-		using shifted = BinIntFalse<and>;
-		using simplified = typename Simplify<shifted>::type;
-		using type = typename Add<xor, simplified>::type;
+		using lengthen = Lengthen<T1, T2>;
+		using type = typename AddInternals::Loop<typename lengthen::type1, typename lengthen::type2, typename lengthen::type2>::type;
 	};
 }
